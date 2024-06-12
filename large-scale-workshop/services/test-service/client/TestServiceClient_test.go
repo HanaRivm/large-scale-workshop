a9bea82da7286be6d92db313fe80f012dadd41d5
hanarivm <hanarivman@mail.tau.ac.il>
2024-06-12 07:08:07 +0000

    more testservice files

---
+++ large-scale-workshop/services/test-service/client/TestServiceClient_test.go
@@ -0,0 +1,28 @@
+package TestService
+
+import (
+	context "context"
+	"testing"
+
+	common "github.com/TAULargeScaleWorkshop/HANA/large-scale-workshop/services/test-service/common"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+func TestHelloWorld(t *testing.T) {
+	conn, err :=
+		grpc.Dial("localhost:50051", grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		t.Fatalf("did not connect: %v", err)
+		return
+	}
+	defer conn.Close()
+	c := common.NewTestServiceClient(conn)
+	// Call the HelloWorld RPC function
+	r, err := c.HelloWorld(context.Background(), &emptypb.Empty{})
+	if err != nil {
+		t.Fatalf("could not call HelloWorld: %v", err)
+		return
+	}
+	t.Logf("Response: %v", r.GetValue())
+}
