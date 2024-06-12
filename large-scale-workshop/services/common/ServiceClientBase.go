f274a359581bf62046ee487a01647838ec63cc50
HanaRivm <hanarivman@mail.tau.ac.il>
2024-06-12 14:17:16 +0000

    new layer

---
+++ large-scale-workshop/services/common/ServiceClientBase.go
@@ -0,0 +1,21 @@
+package common 
+import( 
+    "fmt" 
+    "google.golang.org/grpc" 
+) 
+type ServiceClientBase[client_t any]struct{ 
+    Address      string 
+    CreateClient func(grpc.ClientConnInterface)client_t 
+} 
+func(obj *ServiceClientBase[client_t])Connect() (res client_t,closeFunc 
+func(),err error) { 
+    conn,err :=grpc.Dial(obj.Address,grpc.WithInsecure(),grpc.WithBlock()) 
+    if err !=nil { 
+        var empty client_t 
+        returnempty,nil,fmt.Errorf("failed to connect client to %v: 
+%v",obj.Address,err) 
+    } 
+    c := obj.CreateClient(conn) 
+    returnc,func(){ conn.Close() },nil 
+} 
+ 
---
+++ large-scale-workshop/services/test-service/client/TestServiceClient.go
@@ -0,0 +1,34 @@
+package TestServiceClient
+
+import (
+	services "[module name]/services/common"
+	service "[module name]/services/test-service/common"
+	context "context"
+	"fmt"
+
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+type TestServiceClient struct {
+	services.ServiceClientBase[service.TestServiceClient]
+}
+
+func NewTestServiceClient(address string) *TestServiceClient {
+	return &TestServiceClient{
+
+		ServiceClientBase: services.ServiceClientBase[service.TestServiceClient]{
+			Address:      address,
+			CreateClient: service.NewTestServiceClient,
+		},
+	}
+}
+func (obj *TestServiceClient) HelloWorld() (string, error) {
+	c, closeFunc, err := obj.Connect()
+	defer closeFunc()
+	// Call the HelloWorld RPC function
+	r, err := c.HelloWorld(context.Background(), &emptypb.Empty{})
+	if err != nil {
+		return "", fmt.Errorf("could not call HelloWorld: %v", err)
+	}
+	return r.Value, nil
+}
