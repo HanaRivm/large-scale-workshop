d2be16b1b1712f1dcd7413d66ba17437fdf66f84
hanarivm <hanarivman@mail.tau.ac.il>
2024-06-12 08:24:35 +0000

    more changes

--- large-scale-workshop/services/test-service/service/TestService.go
+++ large-scale-workshop/services/test-service/service/TestService.go
@@ -1,10 +1,14 @@
 package TestService
 
 import (
+	"context"
+
 	services "github.com/TAULargeScaleWorkshop/HANA/large-scale-workshop/services/common"
 	. "github.com/TAULargeScaleWorkshop/HANA/large-scale-workshop/services/test-service/common"
-
+	. "github.com/TAULargeScaleWorkshop/HANA/large-scale-workshop/utils"
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/wrapperspb"
 )
 
 type testServiceImplementation struct {
@@ -16,4 +20,10 @@ func Start(configData []byte) error {
 		RegisterTestServiceServer(s, &testServiceImplementation{})
 	}
 	services.Start("TestService", 50051, bindgRPCToService)
+	return nil
+}
+
+func (obj *testServiceImplementation) HelloWorld(_ context.Context, _ *emptypb.Empty) (res *wrapperspb.StringValue, err error) {
+	Logger.Printf("HelloWorld called")
+	return wrapperspb.String("Hello World"), nil
 }
