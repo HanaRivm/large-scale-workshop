9dfb974ee890feb9c4ae288a22f525d83802ba8b
HanaRivm <hanarivman@mail.tau.ac.il>
2024-06-14 12:34:38 +0000

    testing

--- large-scale-workshop/services/test-service/servant/TestServiceServant.go
+++ large-scale-workshop/services/test-service/servant/TestServiceServant.go
@@ -6,8 +6,6 @@ import (
 	"time"
 
 	metaffi "github.com/MetaFFI/lang-plugin-go/api"
-	"github.com/MetaFFI/plugin-sdk/compiler/go/IDL"
-	"github.com/TAULargeScaleWorkshop/HANA/large-scale-workshop/utils"
 )
 
 var pythonRuntime *metaffi.MetaFFIRuntime
@@ -15,51 +13,42 @@ var crawlerModule *metaffi.MetaFFIModule
 var extract_links_from_url func(...interface{}) ([]interface{}, error)
 var cacheMap map[string]string
 
+func init() {
+	cacheMap = make(map[string]string)
+	// Load the Python3.11 runtime
+	//	pythonRuntime = metaffi.NewMetaFFIRuntime("python311")
+	//	err := pythonRuntime.LoadRuntimePlugin()
+	//	if err != nil {
+	//		msg := fmt.Sprintf("Failed to load runtime plugin: %v", err)
+	//		utils.Logger.Fatalf(msg)
+	//		panic(msg)
+	//	}
+	//
+	//	// Load the Crawler module
+	//	crawlerModule, err = pythonRuntime.LoadModule("./crawler.py")
+	//	if err != nil {
+	//		msg := fmt.Sprintf("Failed to load ./crawler/crawler.py module: %v", err)
+	//		utils.Logger.Fatalf(msg)
+	//		panic(msg)
+	//	}
+	//	// Load the crawler function
+	//	extract_links_from_url, err = crawlerModule.Load("callable=extract_links_from_url",
+	//		[]IDL.MetaFFIType{IDL.STRING8, IDL.INT64}, // parameters types
+	//		[]IDL.MetaFFIType{IDL.STRING8_ARRAY})      // return type
+	//
+	//	if err != nil {
+	//		msg := fmt.Sprintf("Failed to load extract_links_from_url function: %v", err)
+	//		utils.Logger.Fatalf(msg)
+	//		panic(msg)
+	//	}
+}
+
 func HelloWorld() string {
 	return "Hello World"
 }
 
 func HelloToUser(username string) string {
 	return fmt.Sprintf("Hello %s", username)
-}
-
-func init() {
-	cacheMap = make(map[string]string)
-	// Load the Python3.11 runtime
-	pythonRuntime = metaffi.NewMetaFFIRuntime("python311")
-	err := pythonRuntime.LoadRuntimePlugin()
-	if err != nil {
-		msg := fmt.Sprintf("Failed to load runtime plugin: %v", err)
-		utils.Logger.Fatalf(msg)
-		panic(msg)
-	}
-	// Load the Crawler module
-	crawlerModule, err = pythonRuntime.LoadModule("./crawler.py")
-	if err != nil {
-		msg := fmt.Sprintf("Failed to load ./crawler/crawler.py module: %v", err)
-		utils.Logger.Fatalf(msg)
-		panic(msg)
-	}
-	// Load the crawler function
-	extract_links_from_url, err = crawlerModule.Load("callable=extract_links_from_url",
-		[]IDL.MetaFFIType{IDL.STRING8, IDL.INT64}, // parameters types
-		[]IDL.MetaFFIType{IDL.STRING8_ARRAY})      // return type
-
-	if err != nil {
-		msg := fmt.Sprintf("Failed to load extract_links_from_url function: %v", err)
-		utils.Logger.Fatalf(msg)
-		panic(msg)
-	}
-}
-
-func ExtractLinksFromURL(url string, depth int32) ([]string, error) {
-	// Call Python's extract_links_from_url.
-	res, err := extract_links_from_url(url, int64(depth))
-	if err != nil {
-		return nil, err
-	}
-
-	return res[0].([]string), nil
 }
 
 func Store(key, value string) {
@@ -75,3 +64,13 @@ func WaitAndRand(seconds int32, sendToClient func(x int32) error) error {
 	time.Sleep(time.Duration(seconds) * time.Second)
 	return sendToClient(int32(rand.Intn(10)))
 }
+
+//func ExtractLinksFromURL(url string, depth int32) ([]string, error) {
+//	// Call Python's extract_links_from_url.
+//	res, err := extract_links_from_url(url, int64(depth))
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	return res[0].([]string), nil
+//}
