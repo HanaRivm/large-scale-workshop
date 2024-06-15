de537435aa9fa70b2b54dffe485f6ba53e82b32e
hanarivm <hanarivman@mail.tai.ac.il>
2024-06-15 17:50:37 +0000

    registryservice.registry

--- large-scale-workshop/services/registry-service/service/RegistryService.go
+++ large-scale-workshop/services/registry-service/service/RegistryService.go
@@ -73,9 +73,9 @@ func (s *registryServer) startHealthCheck() {
 					continue
 				}
 				client := pb.NewRegistryServiceClient(conn)
-				ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+				_, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 				defer cancel()
-				res, err := client.IsAlive(ctx, &emptypb.Empty{})
+				res, err := client.IsAlive(context.Background(), &emptypb.Empty{})
 				if err != nil {
 					log.Printf("error is not nil")
 					fmt.Println("Error:", err)
