68be14208916ad4c220cd677ee3aed829a7035e6
hanarivm <hanarivman@mail.tai.ac.il>
2024-06-15 22:14:34 +0000

    no chord with cache

---
+++ large-scale-workshop/services/cache-service/service/CacheService.go
@@ -0,0 +1,64 @@
+package CacheService
+
+import (
+	"log"
+	"sync"
+	"time"
+
+	"github.com/TAULargeScaleWorkshop/HANA/large-scale-workshop/services/registry-service/servant/dht"
+)
+
+type CacheService struct {
+	chord *dht.Chord
+	mu    sync.Mutex
+}
+
+func NewCacheService(nodeName string, port int32, rootNodeName string) (*CacheService, error) {
+	var chord *dht.Chord
+	var err error
+	if rootNodeName == "" {
+		chord, err = dht.NewChord(nodeName, port)
+	} else {
+		chord, err = dht.JoinChord(nodeName, rootNodeName, port)
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return &CacheService{
+		chord: chord,
+	}, nil
+}
+
+func (cs *CacheService) Set(key, value string, timeout time.Duration) {
+	cs.mu.Lock()
+	defer cs.mu.Unlock()
+	cs.chord.Set(key, value)
+	if timeout > 0 {
+		go func() {
+			time.Sleep(timeout)
+			cs.chord.Delete(key)
+		}()
+	}
+}
+
+func (cs *CacheService) Get(key string) (string, error) {
+	cs.mu.Lock()
+	defer cs.mu.Unlock()
+	return cs.chord.Get(key)
+}
+
+func (cs *CacheService) Delete(key string) {
+	cs.mu.Lock()
+	defer cs.mu.Unlock()
+	cs.chord.Delete(key)
+}
+
+func (cs *CacheService) IsAlive() bool {
+	alive, err := cs.chord.IsFirst()
+	if err != nil {
+		log.Println("Error checking IsAlive:", err)
+		return false
+	}
+	return alive
+}
